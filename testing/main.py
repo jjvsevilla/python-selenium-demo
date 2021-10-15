e9f08e7edc963a449196183ce35193ed4d48fe78
= <j.j.v.sevilla@gmail.com>
2021-10-15 18:11:40 -0500

    Add instructions for Linux VM

--- testing/main.py
+++ testing/main.py
@@ -4,12 +4,17 @@ from time import sleep
 from getpass import getuser
 
 base_url="http://the-internet.herokuapp.com/upload";
+
+# Use on Linux VM
+# driver_str = "/home/vagrant/python-selenium-demo/testing/driver/current/chromedriver"
+# driver=webdriver.Chrome(executable_path=driver_str)
 driver=webdriver.Chrome(executable_path=f"/Users/{getuser()}/Git/python-selenium-demo/testing/driver/current/chromedriver")
 driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get(base_url)
-
 inputFile = driver.find_element(By.ID, "file-upload")
+# Use on Linux VM
+# csvFile = "/home/vagrant/python-selenium-demo/testing/data/file.csv"
 csvFile = f"/Users/{getuser()}/Git/python-selenium-demo/testing/data/file.csv"
 inputFile.send_keys(csvFile)
 sleep(3)
